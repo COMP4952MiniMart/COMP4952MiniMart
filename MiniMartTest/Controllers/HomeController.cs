b421ecf8a4bfe88dc3b879858446a57fff9d81a3
Gisu Kim <sksmszhdzk@gmail.com>
2017-11-03 14:33:39 -0700

    created project

---
+++ MiniMartTest/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MiniMartTest.Models;
+
+
+namespace MiniMartTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        List<User> userList = new List<Models.User>();
+        public ActionResult Index()
+        {
+             using(var db = new MiniMartDBContentEntities())
+            {
+                var users = db.Users;
+               
+                foreach(User user in users)
+                {
+                    userList.Add(user);
+                }
+            }
+            ViewData["userList"] = userList;
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
